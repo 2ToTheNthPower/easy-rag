dbcb9becf3cd7cdfff499ce1d4ed061510bac473
Aaron Davis <adavi2048@gmail.com>
2024-04-13 15:25:00 -0600

    Improved instructions.  Added start of online model api usage capability.

--- README.md
+++ README.md
@@ -4,20 +4,44 @@ EasyRAG: Easy Retrieval Augmented Generation 🚀
 Overview
 --------
 
-EasyRAG turns your local machine into a powerful AI assistant. By indexing your documents, it enhances language models like llama2, reducing hallucinations and providing more accurate, context-aware responses. Designed for simplicity, it ensures your data stays private, operating entirely offline. 🛡️
+EasyRAG turns your local machine into a powerful AI assistant. By indexing your documents, it enhances language models like llama2 or command R, reducing hallucinations and providing potentially more accurate, context-aware responses. Designed for simplicity, it ensures your data stays private and local. 🛡️
+
+![](https://github.com/2ToTheNthPower/easy-rag/blob/main/resources/demo.gif)
 
 Features
 --------
 
-*   **Local Data Indexing:** Keeps your data grounded and relevant.
-*   **100% Private:** Your data never leaves your machine. 🌐➡️🚫
-*   **Easy Setup:** Straightforward for users on Linux. 🐧
-*   **Fully Offline (After Setup):** Works without an internet connection. 📶🚫
+*   **Local Data Indexing:** Helps keep your LLM grounded and relevant.
+*   **100% Private:** Your data never leaves your machine. 🚫🌐🚫
+*   **Easy Setup:** Straightforward for users on Linux w/ Nvidia GPUs. 🐧
+*   **Fully Offline (After Setup):** Works without an internet connection after downloading models. 🚫📶🚫
 
 Get Started
 -----------
 
+Prerequisites
+-------------
+
+Note that linux is the only supported operating system at the moment.  Porting this to other operating systems is very doable, but requires me to mess around with virtual machine networks and potentially running Ollama outside of my set of containers so accelerated inference will be easier to setup.  If you are interested in porting this to another operating system, please make a pull request with your proposed changes.
+
+Before getting started, make sure you have the following prerequisites installed:
+
+*   **Podman:** Podman is a daemonless container engine for developing, managing, and running OCI Containers. You can install Podman by following the instructions for your specific operating system. Visit the [Podman website](https://podman.io/getting-started/installation) for installation instructions.
+
+*   **Setup Nvidia GPU for accelerated inference:** CDI (Container Device Interface) is a specification that helps containers access your system's hardware. To set up CDI for Nvidia GPU, follow the instructions found [here](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/cdi-support.html).
+
+Installation Steps
+------------------
+
+Follow these steps to get started with EasyRAG:
+
 1.  **Clone:** `git clone https://github.com/2ToTheNthPower/easy-rag.git`
 2.  **Navigate:** `cd easy-rag`
-3.  **Run:** `podman compose up`
-4.  **Visit:** `http://localhost:8501` to configure and start chatting!
+3.  **Add your data:** Add your data to the `data` directory. You can add text files, PDFs, or any other document type you want to index.  Most common text file types are supported by default.
+4.  **(Optional) Setup GPU:** Follow the instructions to set up CDI for Nvidia GPU.  If you choose not to run with a GPU, you can remove the `--device` flag from the `docker-compose.yml
+5.  **Run:** `podman compose up`
+6.  **Visit:** `http://localhost:8501` to download models, index data, and start chatting!
+7.  **Download models:** Click on the "Pull new models" button to download the models you want to use.  This only needs to be done once per model.  The models are stored in the `ollama` directory.  The recommended starting chat model is `command-r:35b-v0.1-q2_K` and the recommended starting embedding model is `nomic-embed-text:latest`.  Both of these models can run on an RTX 3090 with 24GB of VRAM, and command-R is currently one of the best open source chat models for RAG.  If you have a smaller GPU, you may want to try the `llama2` model instead.
+8.  **Index your data:** Click on the "Create data embeddings" button to index the data you placed in the data folder.  This reindexes all the data in the folder every time you click the button, so if you add new data, you will need to click the button again.  Depending on the amount of data and the model you have selected for creating embeddings, this can take a while.
+9.  **Chat:** Start chatting with your AI assistant! 🚀
+
--- frontend/main.py
+++ frontend/main.py
@@ -22,111 +22,118 @@ def response_streamer(response):
 
 # Add button that will request an index from the parser API
 with st.sidebar:
+    local = st.toggle('Run locally :sunglasses:', True)
 
-    st.title("Index all data")
-    local_models = [model["name"] for model in requests.get('http://ollama:11434/api/tags').json()["models"]]
+    if local:
+        local_models = [model["name"] for model in requests.get('http://ollama:11434/api/tags').json()["models"]]
 
-    embedding_model = st.selectbox("Select embedding model", local_models)
+        st.markdown("""---""")
+        st.title("Choose chat model")
+        llm_name = st.selectbox("Select chat model", local_models)
 
-    if st.button("Start"):
-        with st.spinner("Indexing data..."):
-            # Post request to parser:8001/index
-            response = requests.post("http://parser:8001/index", json={"name": embedding_model})
+        st.title("Index all data")
+        embedding_model = st.selectbox("Select embedding model", local_models)
+
+        if st.button("Create data embeddings"):
+            with st.spinner("Indexing data..."):
+                # Post request to parser:8001/index
+                response = requests.post("http://parser:8001/index", json={"name": embedding_model})
+
+        with st.expander("Pull new models"):
+            st.markdown("""---""")
+            st.title("Pull Ollama model")
+            # Allow user to enter model name to pull
+            st.link_button("See options", "https://ollama.com/library")
+            model_to_pull = st.text_input("Enter model name to pull")
+
+            if st.button("Pull model"):
+                with st.spinner("Pulling model..."):
+                    response = requests.post("http://ollama:11434/api/pull", json={"name": model_to_pull})
     
-    st.markdown("""---""")
-    st.title("Choose chat model")
-    llm_name = st.selectbox("Select chat model", local_models)
+    else:
+        st.write("Using remote models is not supported yet")
 
-    st.markdown("""---""")
-    st.title("Pull Ollama model")
-    # Allow user to enter model name to pull
-    st.link_button("See options", "https://ollama.com/library")
-    model_to_pull = st.text_input("Enter model name to pull")
+if local:
+    # bge embedding model
+    # Settings.embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
+    Settings.embed_model = OllamaEmbedding(model_name=embedding_model, base_url="http://ollama:11434")
 
-    if st.button("Pull model"):
-        with st.spinner("Pulling model..."):
-            response = requests.post("http://ollama:11434/api/pull", json={"name": model_to_pull})
+    # ollama
+    Settings.llm = Ollama(model=llm_name, request_timeout=90.0, base_url="http://ollama:11434")
 
-# bge embedding model
-# Settings.embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
-Settings.embed_model = OllamaEmbedding(model_name=embedding_model, base_url="http://ollama:11434")
+    # create the chroma client and add or retrieve our data
+    remote_db = chromadb.HttpClient(host='db', port=8000)
+    chroma_collection = remote_db.get_or_create_collection("quickstart")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 
-# ollama
-Settings.llm = Ollama(model=llm_name, request_timeout=90.0, base_url="http://ollama:11434")
+    index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
-# create the chroma client and add or retrieve our data
-remote_db = chromadb.HttpClient(host='db', port=8000)
-chroma_collection = remote_db.get_or_create_collection("quickstart")
-vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    # Here, a token limit is set
+    memory = ChatMemoryBuffer.from_defaults(llm=Settings.llm)
 
-index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+    # configure retriever
+    retriever = VectorIndexRetriever(
+        index=index,
+        similarity_top_k=5,
+    )
 
-# Here, a token limit is set
-memory = ChatMemoryBuffer.from_defaults(llm=Settings.llm)
+    custom_prompt = PromptTemplate(
+        """\
+    Given a conversation (between Human and Assistant) and a follow up message from Human, \
+    rewrite the message to be a standalone question that captures all relevant context \
+    from the conversation.
 
-# configure retriever
-retriever = VectorIndexRetriever(
-    index=index,
-    similarity_top_k=5,
-)
+    <Chat History>
+    {chat_history}
 
-custom_prompt = PromptTemplate(
-    """\
-Given a conversation (between Human and Assistant) and a follow up message from Human, \
-rewrite the message to be a standalone question that captures all relevant context \
-from the conversation.
+    <Follow Up Message>
+    {question}
 
-<Chat History>
-{chat_history}
+    <Standalone question>
+    """
+    )
 
-<Follow Up Message>
-{question}
+    # list of `ChatMessage` objects
+    custom_chat_history = [
+        ChatMessage(
+            role=MessageRole.USER,
+            content="Hello assistant, today we are coding and/or analysing code.  Pay close attention to the context provided when answering each question.",
+        ),
+        ChatMessage(role=MessageRole.ASSISTANT, content="Okay, sounds good."),
+    ]
 
-<Standalone question>
-"""
-)
+    chat_engine = CondensePlusContextChatEngine.from_defaults(
+        llm=Settings.llm,
+        retriever=retriever,
+        memory=memory,
+        prompt_template=custom_prompt,
+        chat_history=custom_chat_history,
+        response_streamer=response_streamer,
 
-# list of `ChatMessage` objects
-custom_chat_history = [
-    ChatMessage(
-        role=MessageRole.USER,
-        content="Hello assistant, today we are creating animations in Python using the manim library.  Pay close attention to the context provided when answering each question.",
-    ),
-    ChatMessage(role=MessageRole.ASSISTANT, content="Okay, sounds good."),
-]
+    )
 
-chat_engine = CondensePlusContextChatEngine.from_defaults(
-    llm=Settings.llm,
-    retriever=retriever,
-    memory=memory,
-    prompt_template=custom_prompt,
-    chat_history=custom_chat_history,
-    response_streamer=response_streamer,
+    st.title("💬 Chatbot")
 
-)
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-st.title("💬 Chatbot")
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    if prompt := st.chat_input():
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        
+        response = chat_engine.stream_chat(prompt)
+        with st.chat_message("assistant"):
+            msg = st.write_stream(response.response_gen)
 
-if prompt := st.chat_input():
+            with st.expander("See context"):
 
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    
-    response = chat_engine.stream_chat(prompt)
-    with st.chat_message("assistant"):
-        msg = st.write_stream(response.response_gen)
+                sources = [s.node.get_text() for s in response.source_nodes]
+                st.write(sources)
 
-        with st.expander("See context"):
-
-            sources = [s.node.get_text() for s in response.source_nodes]
-            st.write(sources)
-
-    st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.session_state.messages.append({"role": "assistant", "content": msg})
 
     
